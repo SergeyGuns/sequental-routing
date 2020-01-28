9bb0dbbddf3b74008c67621f724af659e089b168
Sergey Ganin <mcheguevara2@gmail.com>
2020-01-28 11:27:58 +0300

    add more router test

--- src/components/router.spec.js
+++ src/components/router.spec.js
@@ -1,50 +1,50 @@
-const Router = require("./router");
-const scenario = require("../test-scenario.json");
+const Router = require('./router');
+const scenario = require('../test-scenario.json');
+const currPresentation = 'galvus-pres';
 const window = {
   document: {
-    location: ""
+    location: ''
   },
 
   sessionStorage: {
     value: {},
-    setItem: (key, value) => (this.value = { ...this.value, [key]: value }),
-    getItem: key => this.value[key] || null
+    setItem(key, value) {
+      return (this.value = { ...this.value, [key]: value });
+    },
+    getItem(key) {
+      return this.value[key] || null;
+    }
   }
 };
-const activeScene = "galvus_met";
-const activeSlide = "2001_verify_019";
-const currPresentation = "galvus-pres";
+describe('Router.findNextSlide() | .findPrevSlide() .goNext()', () => {
+  test('router 1', () => {
+    const router1 = new Router({
+      activeScene: 'galvus_met',
+      activeSlide: '2001_verify_019',
+      scenario,
+      window,
+      currPresentation
+    });
 
-const router1 = new Router({
-  activeScene,
-  activeSlide,
-  scenario,
-  window,
-  currPresentation
+    expect(router1.findNextSlide()).toBe('2001_verify_021');
+    expect(router1.findPrevSlide()).toBe('1904_012');
+    router1.goNext();
+    expect(router1.activeSlide).toBe('2001_verify_021');
+  });
 });
 
-console.log(router1);
-console.log(router1.getActiveSlideScene());
-console.log(
-  "router.findNextSlide() => ",
-  router1.findNextSlide(),
-  "=== 2001_verify_021",
-  router1.findNextSlide() === "2001_verify_021" ? "OK" : "ERR"
-);
-console.log(
-  "router.findPrevSlide() => ",
-  router1.findPrevSlide(),
-  "=== 1904_012",
-  router1.findPrevSlide() === "1904_012" ? "OK" : "ERR"
-);
-const router2 = new Router({
-  activeScene: "galvus_met",
-  activeSlide: "2001_verify_021",
-  scenario,
-  window,
-  currPresentation
+describe('Router.goNext() :: to other scene by rib from last slide', () => {
+  test('router 2', () => {
+    const router2 = new Router({
+      activeScene: 'galvus_met',
+      activeSlide: '2001_verify_021',
+      scenario,
+      window,
+      currPresentation
+    });
+    router2.goNext();
+    expect(router2.window.document.location).toBe('galvus-pres/galvus-pres_1904_010.html');
+    expect(router2.activeSlide).toBe('1904_010');
+    expect(router2.activeScene).toBe('pacientka_1');
+  });
 });
-
-console.log(router2);
-router2.goNext();
-console.log(router2);
